87fe6b4a44964a39542fed0f25c49337088f1907
Kenny Zhao <haise.zhao@gmail.com>
2022-12-13 18:37:53 -0800

    Add function that calls previous operation functions

--- scripts.js
+++ scripts.js
@@ -20,3 +20,23 @@ function divide(a, b) {
     }
     return a / b;
 }
+
+//Function that takes in an operator and two numbers as arguments and calls the appropriate operation to perform on the numbers
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+        default:
+            return "Error."
+    }
+}
